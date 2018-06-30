51b2b45ec7f7dd6314c22586ba38d1957f6e3828
landontholen <tholenlandon@gmail.com>
2018-06-30 16:35:18 -0600

    Created file skeleton

---
+++ index.js
@@ -0,0 +1,20 @@
+var bodyParser = require("body-parser"),
+    expressSanitizer = require("express-sanitizer"), 
+    methodOverride = require("method-override"), 
+//    mongoose = require("mongoose"),
+    express = require("express"),
+    app = express(); 
+
+//mongoose.connect("mongod://localhost/winenthings"); 
+
+app.set("view engine", "ejs"); 
+app.use(express.static("public")); 
+app.use(bodyParser.urlencoded({extended: true})); 
+app.use(expressSanitizer()); 
+app.use(methodOverride("_method")); 
+
+app.get("/", function(req, res){
+    res.render("index"); 
+}); 
+
+app.listen(3000, ()=> console.log("The Wine'N'Things server has been started.")); 
